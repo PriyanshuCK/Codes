2786ac33a819bc3dd7365c57ad010cb5f39e0633
PriyanshuCK <priyanshu.sharma.rampura@gmail.com>
2023-04-30 15:04:35 +0530

    Day 01 - 300423

--- Day 01 - 300423/Kattis - yinyangstones.cpp
+++ Day 01 - 300423/Kattis - yinyangstones.cpp
@@ -15,14 +15,7 @@ int main()
     int w = 0, b = 0;
     for (size_t i = 0; i < s.length(); i++)
     {
-        if (s[i] == 'W')
-        {
-            w++;
-        }
-        else
-        {
-            b++;
-        }
+        s[i] == 'W' ? w++ : b++;
     }
 
     cout << (w == b ? 1 : 0) << endl;
